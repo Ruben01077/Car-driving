53111763b9438626eff234df47371fe5a2ee38a3
Ruben01077 <102193579+Ruben01077@users.noreply.github.com>
2022-08-05 09:26:54 -0700

    Add files via upload

---
+++ newInventory.js
@@ -0,0 +1,12 @@
+function newInventory(){
+    let inventory = document.createElement('div')
+    inventory.style.width = '100%'
+    inventory.style.height = '100px'
+    inventory.style.display = 'flex'
+    inventory.style.flexDirection = 'row'
+    inventory.style.alignItems = 'center'
+    inventory.style.justifyContent = 'space-evenly'
+ 
+    document.body.append(inventory)
+    return inventory
+}
